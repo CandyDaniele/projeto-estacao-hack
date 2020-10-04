1e8dbc13ceabe6e1dd3c81b4e64b23a960b314cb
letcamara <lettcamara@gmail.com>
2020-10-04 12:47:37 -0300

    Estilização da mensagem de CEP inválido

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('formId');
+const header = document.querySelector('header')
 const inputCep = document.getElementById('cepId');
 const inputMedicine = document.getElementById('medicineId');
 
@@ -12,7 +13,14 @@ form.addEventListener('submit', async (evt) => {
   if(!resultCep.erro){
   window.location = `filtro.html?uf=${resultCep.uf}&city=${resultCep.localidade}&medicine=${medicine}`;
   }else{
-    alert("erro");
+    header.innerHTML += `
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>Ops!</strong> Coloque um CEP válido.
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    `
   }
 });
 
