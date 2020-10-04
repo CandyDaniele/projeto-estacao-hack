e0ded4c3774b8cf8947df3992122228801fb69b4
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-10-03 22:43:27 -0300

    feat(filtro.js): fetch api medicine

--- js/filtro.js
+++ js/filtro.js
@@ -1,7 +1,23 @@
-window.addEventListener('load', () => {
+window.addEventListener('load', async() => {
   let query = new URLSearchParams(location.search);
 
   let ufQuery = query.get('uf');
   let cityQuery = query.get('city');
   let medicineQuery = query.get('medicine');
+
+  const resultMedicine = await getMedicines(medicineQuery);
+
+  resultMedicine.data.forEach(medicine => {
+    console.log(medicine);
+  });
+
+
 });
+
+async function getMedicines(medicine){
+  const url = `https://api-medicine-brazil.herokuapp.com/medicines?name=${medicine}`;
+
+  const response = await fetch(url);
+  const result = await response.json();
+  return result;
+}
