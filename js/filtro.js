6b195b9f2d92bbed68dc91fc3903ded55b16b402
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-10-04 00:33:41 -0300

    feat(filtro.js): control result api medicine

--- js/filtro.js
+++ js/filtro.js
@@ -21,21 +21,25 @@ async function getMedicines(medicine){
 }
 
 function renderMedicineCard(resultMedicine) {
-  titleMedicine.innerHTML += medicineQuery;
-  let stringInnerHTML = '';
-  resultMedicine.data.forEach(medicine => {
-    stringInnerHTML = '<a href="./dados.html" class="card">';
-    stringInnerHTML +=  '<div class="card-body">';
-    stringInnerHTML +=  `<h5 class="card-title">${medicine.produto}</h5>`;
-    stringInnerHTML +=  `<p class="ph__remedy-value-notax card-text">R$ ${medicine.pmc12}</p>`;
-    stringInnerHTML +=  '<ul class="ph__remedy-components mb-0">';
-    stringInnerHTML +=  `<li class="ph__remedy-composition">${medicine.substancia}</li>`;
-    stringInnerHTML +=  `<li class="ph__remedy-laboratory">${medicine.laboratorio}</li>`;
-    stringInnerHTML +=  `<li class="ph__remedy-hospital">Restrito a Hospital: ${medicine.hospitalar?"Sim":"Não"}</li>`;
-    stringInnerHTML +=  `<li class="ph__remedy-targe">Tarja: ${medicine.tarja}</li>`;
-    stringInnerHTML +=  '</ul>';
-    stringInnerHTML +=  '</div>';
-    stringInnerHTML +=  '</a>';
-    divCard.innerHTML += stringInnerHTML;
-  });
+  if(resultMedicine.message){
+    titleMedicine.innerHTML += resultMedicine.message;
+  }else{
+    titleMedicine.innerHTML += medicineQuery;
+    let stringInnerHTML = '';
+    resultMedicine.data.forEach(medicine => {
+      stringInnerHTML = '<a href="./dados.html" class="card">';
+      stringInnerHTML +=  '<div class="card-body">';
+      stringInnerHTML +=  `<h5 class="card-title">${medicine.produto}</h5>`;
+      stringInnerHTML +=  `<p class="ph__remedy-value-notax card-text">R$ ${medicine.pmc12}</p>`;
+      stringInnerHTML +=  '<ul class="ph__remedy-components mb-0">';
+      stringInnerHTML +=  `<li class="ph__remedy-composition">${medicine.substancia}</li>`;
+      stringInnerHTML +=  `<li class="ph__remedy-laboratory">${medicine.laboratorio}</li>`;
+      stringInnerHTML +=  `<li class="ph__remedy-hospital">Restrito a Hospital: ${medicine.hospitalar?"Sim":"Não"}</li>`;
+      stringInnerHTML +=  `<li class="ph__remedy-targe">Tarja: ${medicine.tarja}</li>`;
+      stringInnerHTML +=  '</ul>';
+      stringInnerHTML +=  '</div>';
+      stringInnerHTML +=  '</a>';
+      divCard.innerHTML += stringInnerHTML;
+    });
+  }
 }
