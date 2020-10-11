9c050b1c936dda1dfc134e2dd0361fadb6695773
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-10-11 01:37:34 -0300

    feat(dados): insert data dynamically

---
+++ js/dados.js
@@ -0,0 +1,19 @@
+let commercialName = document.getElementById('nome');
+let tarja = document.getElementById('tarja');
+let maxValue = document.getElementById('maxValue');
+let principioAtivo = document.getElementById('principioAtivo');
+let valueWithoutTax = document.getElementById('valueWithoutTax');
+let lab = document.getElementById('lab');
+let hospital = document.getElementById('hospital');
+
+window.addEventListener("load", () => {
+  let medicine = JSON.parse(sessionStorage.getItem("medicine"));
+
+  commercialName.innerText = medicine.produto;
+  tarja.innerText = medicine.tarja;
+  maxValue.innerText = medicine.finalValue;
+  principioAtivo.innerText = medicine.substancia;
+  valueWithoutTax.innerText = 'R$ ' + medicine.semimposto;
+  lab.innerText = medicine.laboratorio;
+  hospital.innerText = medicine.hospitalar? 'Sim' : 'Não';
+});
