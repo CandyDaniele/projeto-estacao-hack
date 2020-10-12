4fa4d4f3ea4486e1cd9f5b32154d96d671468db5
Lara Vieira <laravieiracoelho@hotmail.com>
2020-10-12 12:01:42 -0300

    feat: Ready loading creation function

--- js/loading-system.js
+++ js/loading-system.js
@@ -1,1 +1,14 @@
-let oi ="oi";
+const createLoading = (IdDivPaiDosDados) =>{
+
+    const pai = document.getElementById(IdDivPaiDosDados);
+    const loading = document.createElement("div");
+    loading.className = "spinner-border text-success";
+    loading.id = "loading";
+    loading.role = "status";
+    const message = document.createElement("span");
+    const messageText = document.createTextNode("Carregando...");
+    message.appendChild(messageText);
+    loading.appendChild(message);
+    pai.appendChild(loading);
+
+}
